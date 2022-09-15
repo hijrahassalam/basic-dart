425c3bbfeba80a082dbd3b77c1bf537a33d26753
hijrahassalam <hijrahassalam@student.uns.ac.id>
2022-09-15 21:11:26 +0700

    commenting and adding boolean variables

--- variables.dart
+++ variables.dart
@@ -1,4 +1,10 @@
 void main() {
+  // this is one line comment
+
+  /*  this is 
+      mulitline comment
+  */
+
   var name = 'Voyager I';
   var year = 1977;
   var antennaDiameter = 3.7;
@@ -7,10 +13,19 @@ void main() {
     'tags': ['saturn'],
     'url': '//path/to/saturn.jpg'
   };
+  var isRun = true;
 
+  // printing a string
   print('name : ${name}');
+  // printing an integer
   print('this year : $year');
   print(antennaDiameter);
-  print(flybyObjects);
+  //printing an array to string
+  print('this is array ${flybyObjects}');
+  // printing 1st index of array
+  print('this is array ${flybyObjects[0]}');
+  // printing a map (key => value)
   print(image);
+  // printing a boolean
+  print(isRun);
 }
